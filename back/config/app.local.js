67dc47865628f7c75c245412f210ef722a1b2ae2
liao1a <153887111@qq.com>
2020-07-28 09:49:16 +0800

    定制配置

---
+++ back/config/app.local.js
@@ -0,0 +1,36 @@
+let appConfig = {
+  router: {
+    auth: {
+      prefix: 'messenger/auth', // 鉴权接口调用url的前缀
+    },
+    controllers: {
+      prefix: 'messenger/api', // 接口调用url的前缀
+    },
+    fsdomain: {
+      prefix: 'messenger/fs', // 文件下载服务的前缀
+    },
+  },
+  auth: {
+    disabled: true,
+    jwt: {
+      privateKey: 'tms-messenger',
+      expiresIn: 3600,
+    },
+    captcha: {
+      code: '1234',
+    },
+    //
+    client: {
+      accounts: [
+        // 默认用户组
+        {
+          id: 1,
+          username: 'root',
+          password: 'root',
+        },
+      ],
+    },
+  },
+}
+
+module.exports = appConfig
